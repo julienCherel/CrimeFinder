297fedf542e92ee1d95794761ccb0cef366bb6aa
Unknown <julien.cherel@etu.univ-montp3.fr>
2017-11-29 16:37:12 +0100

    javascript dans mapics

--- mapics.js
+++ mapics.js
@@ -1,93 +1,14 @@
-var osmUrl = 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
-stamenUrl = 'https://{s}.tiles.mapbox.com/v3/mslee.h1kk2o6r/{z}/{x}/{y}.png',
-main = L.tileLayer(stamenUrl, {maxZoom: 18}),
-map = new L.Map('map', {
-	layers: [main],
-	center: [43.65009,3.57468],
-	zoom: 10 
-}),
-overlay = L.tileLayer(osmUrl).addTo(map);
-overlay.getContainer().style.display = "none";
 
-var drawnItems = new L.FeatureGroup(); //group des polygones
-map.addLayer(drawnItems);
+    // ajoute la map au div "map"
+    var map = L.map('map');
+    id='satellite-streets-v9'
+    token='pk.eyJ1IjoianVsaWVuMTAxMCIsImEiOiJjajk5cHpkdjAxMHA1MzNuMm12aWd1OW5nIn0.DzPgmtGDquEzt1IyVb7yJQ'
+    L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/'+id+'/tiles/{z}/{x}/{y}?access_token='+token).addTo(map);
 
-var drawControl = new L.Control.Draw({
-	draw: {
-		position: 'topleft',
-		polygon: false,
-		rectangle: false,		
-		polyline: {
-			shapeOptions: {
-				color: '#fff',
-				weight: 2,
-				opacity: 0.8,
-				fill: false
-				}
-			},
-		marker: false,
-		circle: {
-			shapeOptions: {
-				color: '#fff',
-				weight: 2,
-				opacity: 0.8,
-				fill: false
-			}
-		}	
-	},
-	edit: {
-		featureGroup: drawnItems,
-		remove: true,
-		buffer: {
-		  replacePolylines: false,
-		  separateBuffer: false,
-		},
-	},
-});
-map.addControl(drawControl);
+    map.setView([54.5,-3], 6);
 
-map.on('draw:created', function (e) {
-	layer = e.layer; //layer = polygone dessiné
-	drawnItems.addLayer(layer);
-});
-
-	/*
-//Clipping or masking change code accordingly
-function clip() {
-	var clippingPaths =  clipPath.selectAll("path");
-	clippingPaths.attr("d", path);
-	
-	if (clippingPaths.size() > 0) {
-		overlay.getContainer().style.display = "inline";
-		overlay.getContainer().style.clipPath = 'url(#clipPath)';
-		overlay.getContainer().style.WebkitClipPath = 'url(#clipPath)';
-	} else {
-		overlay.getContainer().style.display = "none";
-		overlay.getContainer().style.clipPath = 'none';
-		overlay.getContainer().style.WebkitClipPath = 'none';
-	};
-};
-
-
-
-*/
-map.on('dblclick',function (e) {
-	console.log(drawnItems._layers);
-  });
-/*
-  map.on('dblclick',function (e) {
-	for (var element in drawnItems._layers){
-		for (var elt in drawnItems._layers[element]){
-			console.log(`drawnItems._layers[element].${elt} = ${drawnItems._layers[element][elt]}`);
-		}
-	}
-  });
-
-  map.on('dblclick',function (e) {
-	for (var element in drawnItems._layers){
-		for (var elt in drawnItems._layers[element]){
-			console.log(JSON.stringify(elt));
-		}
-	}
-  });
-  */
+    map.on('dblclick', function(e){
+         var marker = new L.marker([54.6357, -6.48193]);
+         marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup(); //dans bindPopup mettre le texte de l'infoBulle en html
+         marker.addTo(map);
+    });
