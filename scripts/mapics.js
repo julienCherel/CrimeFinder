faf14adff634d385aa2252176f8c8b9489282149
Unknown <julien.cherel@etu.univ-montp3.fr>
2017-11-30 15:31:57 +0100

    marker on click, lat lng

--- scripts/mapics.js
+++ scripts/mapics.js
@@ -7,8 +7,11 @@
 
     map.setView([54.5,-3], 6);
 
-    map.on('dblclick', function(e){
-         var marker = new L.marker([54.6357, -6.48193]);
-         marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup(); //dans bindPopup mettre le texte de l'infoBulle en html
-         marker.addTo(map);
-    });
+    map.on('click', function(e){
+        if (typeof(marker)!='undefined'){
+            map.removeLayer(marker);
+        }
+        document.getElementById("lat").value=e.latlng.lat;
+        document.getElementById("long").value=e.latlng.lng;
+        marker = new L.marker(e.latlng).addTo(map);        
+   });
